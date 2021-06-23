b8cdc0bc133cd62f0e1a984fbd1955a09e3d9a01
Alastair Smith <j.alastair.smith@outlook.com>
2021-06-23 21:30:23 +0100

    adds image source

--- src/components/ChartEntry.js
+++ src/components/ChartEntry.js
@@ -4,7 +4,8 @@ const ChartEntry = ({song}) => {
     return(
         <li>
             <h3>{song['im:name'].label}</h3>
-            by: {song['im:artist'].label}
+            <p>by: {song['im:artist'].label}</p>
+            <img src={song['im:image'][1].label} alt='album artwork'></img>
         </li>
     )
 }
