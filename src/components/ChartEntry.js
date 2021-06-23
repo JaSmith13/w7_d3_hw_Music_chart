4a1f5c36126c63f909cdfe4ca4aecddb49698c65
Alastair Smith <j.alastair.smith@outlook.com>
2021-06-23 21:30:39 +0100

    adds audio player to site

--- src/components/ChartEntry.js
+++ src/components/ChartEntry.js
@@ -1,11 +1,19 @@
 
 const ChartEntry = ({song}) => {
 
+    // const previewSong = (event) => {
+
+    // }
+
     return(
         <li>
             <h3>{song['im:name'].label}</h3>
             <p>by: {song['im:artist'].label}</p>
             <img src={song['im:image'][1].label} alt='album artwork'></img>
+            <br></br>
+            <audio controls>
+                <source src={song.link[1].attributes.href} />
+            </audio>
         </li>
     )
 }
